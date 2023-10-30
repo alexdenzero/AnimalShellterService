d49efe01074451659590eb4346389a86c02c0d47
alexdenzero <125552476+alexdenzero@users.noreply.github.com>
2023-10-30 10:28:04 +0300

    start

---
+++ src/main/java/pro/sky/animalizer/AnimalizerApplication.java
@@ -0,0 +1,12 @@
+package pro.sky.animalizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalizerApplication.class, args);
+    }
+}
