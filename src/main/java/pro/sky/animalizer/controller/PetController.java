54534cd55f9d653b2dd6dc44ecd55f7b829c6068
internal-syntax <sandro-wold@live.ru>
2023-12-04 23:12:41 +0500

    1. Добавлен enum ButtonType 2. Добавлен enum ShelterType 3. В ShelterController исключен импорт import io.swagger.v3.oas.annotations.parameters.RequestBody;, в описании эндпоинтов указал полный путь до аннотации RequestBody (requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody) 4. В public class Pet добавил аннотацию с параметрами @ManyToOne @JoinColumn(name = "pet_owner_id") private User petOwner; 5. В public class User добавил аннотацию с параметрами @OneToMany(mappedBy = "petOwner", cascade = CascadeType.ALL, orphanRemoval = true) private Set<Pet> pets = new HashSet<>(); 6. Добавил контроллер public class PetController

---
+++ src/main/java/pro/sky/animalizer/controller/PetController.java
@@ -0,0 +1,154 @@
+package pro.sky.animalizer.controller;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pro.sky.animalizer.model.Pet;
+import pro.sky.animalizer.service.PetService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pet")
+public class PetController {
+
+    private final PetService petService;
+
+    @Autowired
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
+
+    @Operation(
+            summary = "Поиск всех питомцев в базе данных",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Коллекция всех питомцев, существующих в базе данных",
+                            content = {@Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    array = @ArraySchema(schema = @Schema(implementation = Pet[].class))
+                            )
+                            }
+                    )
+            })
+    @GetMapping("/{pets}")
+    public List<Pet> getAllPets() {
+        return petService.getAllPet();
+    }
+
+    @Operation(
+            summary = "Поиск питомца по идентификатору",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Питомец, найденный по идентификатору",
+                            content = {@Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Pet.class)
+                            )
+                            }
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Питомца с переданным id не существует"
+                    )
+            })
+    @GetMapping("/{id}")
+    public Pet findPetById(@PathVariable Long id) {
+        return petService.getPetById(id);
+    }
+
+    @Operation(
+            summary = "Добавление питомца в базу данных",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Добавляемый питомец",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Pet.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Питомец добавлен в базу данных",
+                            content = {@Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Pet.class)
+                            )
+                            }
+                    )
+            })
+    @PostMapping
+    public Pet createPet(@RequestBody Pet pet) {
+        return petService.createPet(pet);
+    }
+
+    @Operation(
+            summary = "Изменение питомца в базе данных по искомому идентификатору",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Отредактированный питомец",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Pet.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Питомец с переданным илентификатором изменен",
+                            content = {@Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Pet.class)
+                            )
+                            }
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Питомец с переданным id не существует"
+                    )
+            })
+    @PutMapping("/{id}")
+    public ResponseEntity<Pet> editPet(@Parameter(description = "Идентификатор для поиска") @PathVariable Long id,
+                                       @RequestBody Pet pet) {
+        Pet editedPet = petService.editPet(id, pet);
+        if (editedPet == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(editedPet);
+    }
+
+    @Operation(
+            summary = "Удаление питомца по идентификатору",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Питомец с переданным илентификатором удален",
+                            content = {@Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Pet.class)
+                            )
+                            }
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Питомец с переданным id не существует"
+                    )
+            })
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Pet> removePet(@Parameter(description = "Идентификатор для поиска") @PathVariable Long id) {
+        Pet deletedPet = petService.deletePet(id);
+        if (deletedPet == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(deletedPet);
+    }
+}
